dace219466bf83a8469bae5df113d6e8e80dccbf
abin c joseph <abincjoseph77@gmail.com>
2024-11-09 13:17:46 +0530

    best seller

---
+++ src/Book.js
@@ -0,0 +1,13 @@
+const Book = (props) => {
+    const {image,title,autor,number } = props; //<img src={props.image} alt={props.title} /> Reusage props is reduce while using this code 
+    console.log(number);
+    return (
+            <article className='book'>
+                <img src={image} alt={props.title} />
+                <h2>{title}</h2>
+                <h4>{autor.toUpperCase()}</h4>
+                <span className="number">{`# ${number + 1}`}</span>
+            </article>
+    );
+};
+export default Book;
---
+++ src/books.js
@@ -0,0 +1,52 @@
+import img1 from './images/book1.webp'
+import img2 from './images/book2.webp'
+import img3 from './images/book3.webp'
+import img4 from './images/book4.webp'
+import img5 from './images/book5.webp'
+import img6 from './images/book6.webp'
+
+
+export const books = [
+    {
+        title : 'King of Wrath (Kings of Sin Book 1)',
+        autor : 'by Ana Huang',
+        image : img1,
+        id:1
+    },
+    {
+        title : 'The Striker (Gods of the Game Book 1)',
+        autor : 'by Ana Huang',
+        image : img2,
+        id:2
+    },
+    {
+        title : 'The Lost Bookshop: The most charming and uplifting novel for 2024 and the perfect gift for book lovers!',
+        autor : 'by Evie Woods ',
+        image : img3,
+        id:3
+    },
+    {
+        title : 'The Body in the Backyard',
+        autor : 'by Lucy Score',
+        image : img4,
+        id:4
+    },
+    {
+        title : 'The Women: A Novel ',
+        autor : 'Kristin Hannah',
+        image : img5,
+        id:5
+    },
+    {
+        title : 'Counting Miracles: A Novel ',
+        autor : 'Nicholas Sparks (Author, Narrator), Holter Graham (Narrator), Random House Audio (Publisher)',
+        image : img6,
+        id:6
+    },
+    // {
+    //     title : 'Counting Miracles: A Novel ',
+    //     autor : 'Nicholas Sparks (Author, Narrator), Holter Graham (Narrator), Random House Audio (Publisher)',
+    //     image : './images/book6.webp', (the removal of the image or images on src folder is more faster in application bult in )
+    //     id:6
+    // },
+];
